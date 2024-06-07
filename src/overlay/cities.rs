5d67b577ef60ccee9da6db730ffa78d39994f3d1
Moritz Baron <r.fuzzo@gmail.com>
2024-06-07 16:39:26 +0200

    fix cities colors
    closes #7

--- src/overlay/cities.rs
+++ src/overlay/cities.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 use eframe::epaint::Stroke;
-use egui::{Color32, emath::RectTransform, Pos2, Rect, Rounding, Shape};
+use egui::{emath::RectTransform, Color32, Pos2, Rect, Rounding, Shape};
 use tes3::esp::Cell;
 
-use crate::{CellKey, dimensions::Dimensions};
+use crate::{dimensions::Dimensions, CellKey};
 
 pub fn get_cities_shapes(
     to_screen: RectTransform,
@@ -12,7 +12,7 @@ pub fn get_cities_shapes(
     cell_records: &HashMap<CellKey, Cell>,
 ) -> Vec<Shape> {
     let cell_size = dimensions.cell_size();
-
+    let color = Color32::from_rgb(202, 165, 96);
     let shapes_len =
         (dimensions.max_x - dimensions.min_x + 1) * (dimensions.max_y - dimensions.min_y + 1);
     let mut shapes: Vec<Shape> = Vec::with_capacity(shapes_len as usize);
@@ -22,22 +22,21 @@ pub fn get_cities_shapes(
             // get region
             let key = (cx, cy);
             if let Some(cell) = cell_records.get(&key) {
-                if let Some(map_color) = cell.map_color {
-                    let color = Color32::from_rgb(map_color[0], map_color[1], map_color[2]);
-
-                    let p00x = cell_size * dimensions.tranform_to_canvas_x(cx);
-                    let p00y = cell_size * dimensions.tranform_to_canvas_y(cy);
-                    let p00 = Pos2::new(p00x as f32, p00y as f32);
-
-                    let p11x = p00x + cell_size;
-                    let p11y = p00y + cell_size;
-                    let p11 = Pos2::new(p11x as f32, p11y as f32);
-
-                    let rect = Rect::from_two_pos(to_screen * p00, to_screen * p11);
-                    let shape =
-                        Shape::rect_stroke(rect, Rounding::default(), Stroke::new(4.0, color));
-                    shapes.push(shape);
+                if cell.name.is_empty() || cell.is_interior() {
+                    continue;
                 }
+
+                let p00x = cell_size * dimensions.tranform_to_canvas_x(cx);
+                let p00y = cell_size * dimensions.tranform_to_canvas_y(cy);
+                let p00 = Pos2::new(p00x as f32, p00y as f32);
+
+                let p11x = p00x + cell_size;
+                let p11y = p00y + cell_size;
+                let p11 = Pos2::new(p11x as f32, p11y as f32);
+
+                let rect = Rect::from_two_pos(to_screen * p00, to_screen * p11);
+                let shape = Shape::rect_stroke(rect, Rounding::default(), Stroke::new(4.0, color));
+                shapes.push(shape);
             }
         }
     }
